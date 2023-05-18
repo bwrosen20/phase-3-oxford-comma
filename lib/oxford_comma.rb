9aaabcc96742c38faf25113a88c944e1760504f0
bwrosen20 <bwrosen20@gmail.com>
2023-05-18 19:03:37 -0400

    commit tree bed472c0eb5da0583f2620a8400f816c7a721737

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
-def oxford_comma(array)
 
+def oxford_comma(array)
+    if array.length<=1
+        comma_array=array.pop
+    elsif array.length==2
+        comma_array = array.join(" and ")
+    else
+        last_element=array.pop
+        comma_array=array.join(", ")
+        comma_array << ", and #{last_element}"
+    end
+    comma_array
 end
